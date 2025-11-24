7b27a0aa537a4ea45411fe8c8c2873ba176c8a3c
Daniel Abraham <aeroquix@gmail.com>
2025-11-23 19:22:16 -0800

    feat(slack): return file metadata when upload is complete

--- pkg/slack/files.go
+++ pkg/slack/files.go
@@ -58,13 +58,16 @@ type FilesCompleteUploadExternalRequest struct {
 type FilesCompleteUploadExternalResponse struct {
 	Response
 
-	Files []map[string]any `json:"files,omitempty"`
+	Files []File `json:"files,omitempty"`
 }
 
 // https://docs.slack.dev/reference/methods/files.completeuploadexternal/
-func FilesCompleteUploadExternalActivity(ctx workflow.Context, fileID, title, channelID, threadTS string) error {
-	req := FilesCompleteUploadExternalRequest{Files: []File{{ID: fileID, Title: title}}, ChannelID: channelID, ThreadTS: threadTS}
-	return internal.ExecuteTimpaniActivity(ctx, FilesCompleteUploadExternalActivityName, req).Get(ctx, nil)
+func FilesCompleteUploadExternalActivity(ctx workflow.Context, req FilesCompleteUploadExternalRequest) ([]File, error) {
+	resp := new(FilesCompleteUploadExternalResponse)
+	if err := internal.ExecuteTimpaniActivity(ctx, FilesCompleteUploadExternalActivityName, req).Get(ctx, resp); err != nil {
+		return nil, err
+	}
+	return resp.Files, nil
 }
 
 type TimpaniUploadExternalRequest struct {
@@ -78,8 +81,59 @@ func TimpaniUploadExternalActivity(ctx workflow.Context, url, mimeType string, c
 	return internal.ExecuteTimpaniActivity(ctx, TimpaniUploadExternalActivityName, req).Get(ctx, nil)
 }
 
-// https://docs.slack.dev/reference/methods/files.completeuploadexternal/
+// https://docs.slack.dev/reference/objects/file-object/#types
 type File struct {
 	ID    string `json:"id,omitempty"`
 	Title string `json:"title,omitempty"`
+
+	Created      int    `json:"created,omitempty"`
+	Updated      int    `json:"updated,omitempty"`
+	Size         int    `json:"size,omitempty"`
+	Name         string `json:"name,omitempty"`
+	FileType     string `json:"filetype,omitempty"`
+	MimeType     string `json:"mimetype,omitempty"`
+	PrettyType   string `json:"pretty_type,omitempty"`
+	ExternalType string `json:"external_type,omitempty"`
+
+	UserTeam string `json:"user_team,omitempty"`
+	User     string `json:"user,omitempty"`
+
+	Channels []string `json:"channels,omitempty"`
+	Groups   []string `json:"groups,omitempty"`
+	IMs      []string `json:"ims,omitempty"`
+	PinnedTo []string `json:"pinned_to,omitempty"`
+
+	Mode       string `json:"mode,omitempty"`
+	FileAccess string `json:"file_access,omitempty"`
+	Lines      int    `json:"lines,omitempty"`
+	LinesMore  int    `json:"lines_more,omitempty"`
+
+	Editable           bool `json:"editable,omitempty"`
+	HasMoreShares      bool `json:"has_more_shares,omitempty"`
+	HasRichPreview     bool `json:"has_rich_preview,omitempty"`
+	IsExternal         bool `json:"is_external,omitempty"`
+	IsPublic           bool `json:"is_public,omitempty"`
+	PreviewIsTruncated bool `json:"preview_is_truncated,omitempty"`
+	PublicURLShared    bool `json:"public_url_shared,omitempty"`
+
+	EditLink           string `json:"edit_link,omitempty"`
+	Permalink          string `json:"permalink,omitempty"`
+	PermalinkPublic    string `json:"permalink_public,omitempty"`
+	URLPrivate         string `json:"url_private,omitempty"`
+	URLPrivateDownload string `json:"url_private_download,omitempty"`
+
+	Preview          string `json:"preview,omitempty"`
+	PreviewHighlight string `json:"preview_highlight,omitempty"`
+
+	Shares map[string]map[string][]FileShare `json:"shares,omitempty"` // "private"/"public" -> channel ID -> []FileShare.
+}
+
+type FileShare struct {
+	ChannelName string `json:"channel_name,omitempty"`
+	ShareUserID string `json:"share_user_id,omitempty"`
+	TeamID      string `json:"team_id,omitempty"`
+	TS          string `json:"ts,omitempty"`
+
+	IsSilentShare bool   `json:"is_silent_share,omitempty"`
+	Source        string `json:"source,omitempty"`
 }
